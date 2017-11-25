53cf77a062aac5c570583cc9056fde2d35341884
Rick Wardenburg <slickrickstyle@gmail.com>
2017-11-25 00:12:19 -0600

    kernel/power/powersuspend: new PM kernel driver for Android  w/o early_suspend v1.5

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -24,6 +24,10 @@
 
 #include "mdss_dsi.h"
 
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
+#endif
+
 #define DT_CMD_HDR 6
 
 /* NT35596 panel specific status variables */
@@ -737,6 +741,10 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		return -EINVAL;
 	}
 
+#ifdef CONFIG_POWERSUSPEND
+	set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
+#endif
+
 	pinfo = &pdata->panel_info;
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
@@ -758,6 +766,10 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		if (ctrl->on1_cmds.cmd_cnt)
 			mdss_dsi_panel_cmds_send(ctrl, &ctrl->on1_cmds);
     }
+#endif
+
+#ifdef CONFIG_POWERSUSPEND
+	set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
 #endif
 
 end:
