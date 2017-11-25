abe0f57f99a2bf42b914de5b531955d9db84077e
Rick Wardenburg <slickrickstyle@gmail.com>
2017-11-25 12:42:37 -0600

    update

--- drivers/clk/qcom/clock-gcc-8909.c
+++ drivers/clk/qcom/clock-gcc-8909.c
@@ -1081,21 +1081,23 @@ static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_clk[] = {
 	F( 266670000,	gpll0,		3,	0,	0),
 	F( 307200000,	gpll1_e_gfx3d,	4,	0,	0),
 	F( 409600000,	gpll1_e_gfx3d,	3,	0,	0),
+	F( 465000000,      gpll2,   2,	  0,	0),
 	F_END
 };
 
 static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_465_clk[] = {
-	F( 19200000,	xo,		1,	0,	0),
-	F( 50000000,	gpll0,		16,	0,	0),
-	F( 80000000,	gpll0,		10,	0,	0),
-	F( 100000000,	gpll0,		8,	0,	0),
-	F( 160000000,	gpll0,		5,	0,	0),
-	F( 177780000,	gpll0,		4.5,	0,	0),
-	F( 200000000,	gpll0,		4,	0,	0),
-	F( 266670000,	gpll0,		3,	0,	0),
-	F( 307200000,	gpll1_e_gfx3d,	4,	0,	0),
-	F( 409600000,	gpll1_e_gfx3d,	3,	0,	0),
-	F( 456000000,	gpll2,		2,	0,	0),
+	F(  19200000,	      xo,   1,	  0,	0),
+	F(  50000000,  gpll0,  16,	  0,	0),
+	F(  80000000,  gpll0,  10,	  0,	0),
+	F( 100000000,  gpll0,   8,	  0,	0),
+	F( 160000000,  gpll0,   5,	  0,	0),
+	F( 177780000,  gpll0, 4.5,	  0,	0),
+	F( 200000000,  gpll0,   4,	  0,	0),
+	F( 266670000,  gpll0,   3,	  0,	0),
+	F( 294912000,	   gpll1_e_gfx3d,   3,	  0,	0),
+	F( 310000000,	   gpll2,   3,	  0,	0),
+	F( 400000000,  gpll0,   2,	  0,	0),
+	F( 465000000,      gpll2,   2,	  0,	0),
 	F_END
 };
 
@@ -1108,8 +1110,8 @@ static struct rcg_clk gfx3d_clk_src = {
 	.c = {
 		.dbg_name = "gfx3d_clk_src",
 		.ops = &clk_ops_rcg,
-		VDD_DIG_FMAX_MAP3(LOWER, 200000000, NOMINAL, 307200000, HIGH,
-					409600000),
+		VDD_DIG_FMAX_MAP3(LOW, 100000000, NOMINAL, 310000000, HIGH,
+ +			465000000),
 		CLK_INIT(gfx3d_clk_src.c),
 	},
 };
